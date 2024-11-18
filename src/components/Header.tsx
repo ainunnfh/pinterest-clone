60e6e519fad00177fe0c9e9ecfd278fa22dada03
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-11-18 21:55:58 +0700

    ⭐create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="p-3">
+      <div className="flex justify-between">
+        <div>Logo</div>
+        <div>Home</div>
+        <div>Search</div>
+        <div>Notification Icon</div>
+        <div>Chat Icon</div>
+        <div>Profile Icon</div>
+        <div>Explore</div>
+        <div>Create</div>
+        <div>Arrow</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
