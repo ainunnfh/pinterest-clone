51b8a3e2f11bdba8163d440e1eaf7001e8ff7d36
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-11-20 20:37:07 +0700

    💄update (header): change icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-
+import { IoNotifications } from "react-icons/io5";
+import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
+import { FaPinterest } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { IoIosArrowDown } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="p-3">
       <div className="flex justify-between">
-        <div>Logo</div>
+        <div>
+          <FaPinterest />
+        </div>
         <div>Home</div>
-        <div>Search</div>
-        <div>Notification Icon</div>
-        <div>Chat Icon</div>
-        <div>Profile Icon</div>
         <div>Explore</div>
         <div>Create</div>
-        <div>Arrow</div>
+        <div className="bg-slate-200 rounded-full p-1 w-1/4">
+          <div>
+            <FaSearch color="#8e8e8e" />
+          </div>
+        </div>
+        <div>
+          <IoNotifications />
+        </div>
+        <div>
+          <IoChatbubbleEllipsesSharp />
+        </div>
+        <div>
+          <CgProfile />
+        </div>
+        <div>
+          <IoIosArrowDown />
+        </div>
       </div>
     </div>
   );
