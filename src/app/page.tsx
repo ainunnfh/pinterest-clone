488e3d80a9fe96b23e95c247060eb548f629147d
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-11-22 21:38:22 +0700

    🆙update (page): usage new header comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
+import Header1 from "@/components/Header1";
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Header1 />
     </div>
   );
 }
