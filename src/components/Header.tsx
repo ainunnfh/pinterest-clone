b03ad9fc6cbf694126350fce04622e8e64a7353b
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-11-23 22:12:19 +0700

    💄update (header): change class name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,31 +7,34 @@ import { IoIosArrowDown } from "react-icons/io";
 import { FaSearch } from "react-icons/fa";
 const Header = () => {
   return (
-    <div className="p-3 bg-white shadow-sm h-20 flex items-center justify-around">
-      <div>
-        <FaPinterest className="h-6 w-6 text-red-600" />
-      </div>
-      <div className="font-semibold text-gray-800">Home</div>
-      <div className="font-semibold text-gray-800">Explore</div>
-      <div className="font-semibold text-gray-800">Create</div>
-      <div className="bg-slate-200 rounded-full p-1 w-1/4">
-        <div className="flex gap-1 items-center text-gray-900">
-          <FaSearch color="#8e8e8e" /> Explore
+    <header className="p-3 bg-white shadow-sm h-20 flex ">
+      <nav className="flex gap-4 items-center justify-around w-full">
+        <div>
+          <FaPinterest className="h-6 w-6 text-red-600" />
         </div>
-      </div>
-      <div>
-        <IoNotifications className="h-6 w-6 text-gray-500" />
-      </div>
-      <div>
-        <IoChatbubbleEllipsesSharp className="h-6 w-6 text-gray-500" />
-      </div>
-      <div>
-        <CgProfile className="h-6 w-6 text-gray-500 " />
-      </div>
-      <div>
-        <IoIosArrowDown className="h-6 w-6 text-gray-500" />
-      </div>
-    </div>
+
+        <div className="font-semibold text-gray-800">Home</div>
+        <div className="font-semibold text-gray-800">Explore</div>
+        <div className="font-semibold text-gray-800">Create</div>
+        <div className="bg-slate-200 rounded-full p-1 w-1/4">
+          <div className="flex gap-1 items-center text-gray-900">
+            <FaSearch color="#8e8e8e" /> Explore
+          </div>
+        </div>
+        <div>
+          <IoNotifications className="h-6 w-6 text-gray-500" />
+        </div>
+        <div>
+          <IoChatbubbleEllipsesSharp className="h-6 w-6 text-gray-500" />
+        </div>
+        <div>
+          <CgProfile className="h-6 w-6 text-gray-500 " />
+        </div>
+        <div>
+          <IoIosArrowDown className="h-6 w-6 text-gray-500" />
+        </div>
+      </nav>
+    </header>
   );
 };
 
